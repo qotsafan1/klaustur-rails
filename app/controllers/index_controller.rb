f1e6dd0c00c14c68fdd4f784c0251cca188b5cfc
qotsafan1 <sigurbjornjonss@gmail.com>
2017-01-22 11:33:54 +0000

    More updatess

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,16 +1,5 @@
 class IndexController < ApplicationController
   def index
-    timeNow = DateTime.now
-    timeOfCompetiton = DateTime.new(2017, 5, 27, 12)
-    timeToCompetiton = timeOfCompetiton - timeNow;
-
-    @days = timeToCompetiton.floor.to_i
-    hours = (timeToCompetiton - timeToCompetiton.floor) * 24
-    @hours = hours.floor.to_i
-    minutes = (hours - hours.floor) * 60
-    @minutes = minutes.floor.to_i
-    seconds = (minutes - minutes.floor) * 60
-    @seconds = seconds.floor.to_i
   end
 
   def signup
@@ -23,5 +12,8 @@ class IndexController < ApplicationController
   end
 
   def history
+  end
+
+  def registered
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root to: 'index#index'
-    get 'signup', to: 'index#signup'
-    get 'results', to: 'index#results'    
-    resources :laptimes
-    resources :competitors
-    resources :groups
+    get '/signup', to: 'index#signup'
+    get '/results', to: 'index#results'
+    get '/registered', to: 'index#registered'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
